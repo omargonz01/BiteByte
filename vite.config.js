e2900b8f4ae95cd7cf13d4892adab1651224c0b3
Omar Gonzalez <gomarzalez@gmail.com>
2024-04-17 16:55:20 -0700

    update dependecies + Vite PWA + import manifest

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "BiteByte",
+        short_name: "BiteByte",
+        description:
+          "An app that helps identify macro nutrient information of your food with a snap of a pic.",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/vite.svg",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
