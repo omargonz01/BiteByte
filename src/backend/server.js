4e7de0307cb7de170bdce1f8b085ec69a0bde1a7
Alex Huynh <alexhuynh@Alexs-MacBook-Pro.local>
2024-04-25 12:07:07 -0700

    eliminated env variable and code in url

--- src/backend/server.js
+++ src/backend/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 // Enable CORS for client-side app on a different port or domain
 app.use(cors({
-  origin: [process.env.VITE_APP_FRONTEND_URL || 'http://localhost:5173'],
+  origin: ['https://6629adcaaedc0a268c763f6f--ornate-pavlova-d0d03d.netlify.app' || 'http://localhost:5173'],
 }));
 
 
