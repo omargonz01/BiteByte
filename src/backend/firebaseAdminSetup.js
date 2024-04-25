631a9218e0ef35b44e660d9388f81fe095232a0f
Alex Huynh <alexhuynh@Alexs-MacBook-Pro.local>
2024-04-25 10:40:59 -0700

    fixed method of importing json file

--- src/backend/firebaseAdminSetup.js
+++ src/backend/firebaseAdminSetup.js
@@ -1,22 +1,23 @@
 import admin from 'firebase-admin';
+import { createRequire } from 'module';
 
+const require = createRequire(import.meta.url);
+
+// Check if the environment variable is set for the Firebase service account
 let serviceAccount;
 
 if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-  // Decode the base64 environment variable to an object
-  const serviceAccountDecoded = Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, 'base64').toString('ascii');
-  serviceAccount = JSON.parse(serviceAccountDecoded);
+  // Decode the base64 environment variable to a string
+  const serviceAccountString = Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, 'base64').toString('ascii');
+  serviceAccount = JSON.parse(serviceAccountString);
 } else {
-  // Dynamically import the JSON file for local development
-  const serviceAccountModule = await import('../../serviceAccountKey.json', {
-    assert: { type: 'json' }
-  });
-  serviceAccount = serviceAccountModule.default;
+  // Local development, use the require function to import the service account JSON
+  serviceAccount = require('../../serviceAccountKey.json');
 }
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://bitebyte-app-default-rtdb.firebaseio.com"
+  databaseURL: "https://your-database-url.firebaseio.com"
 });
 
 export { admin };
