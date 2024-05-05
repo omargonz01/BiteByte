8bea6749fb3ed534a443353fb85b5a1f8df26ad7
Omar Gonzalez <gomarzalez@gmail.com>
2024-05-05 10:12:41 -0700

    update path for manifest.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
         theme_color: "#000000",
         icons: [
           {
-            src: "/src/assets/appIcon.png",
+            src: "/dist/src/assets/appIcon.png",
             sizes: "192x192",
             type: "image/png",
           },
