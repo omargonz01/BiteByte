e5e52b3c9b8bb384a2c11fe5cc187d684ba5a78a
Alex Huynh <alexhuynh@Alexs-MacBook-Pro.local>
2024-05-05 10:33:23 -0700

    added icon to public asset folder

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
         theme_color: "#000000",
         icons: [
           {
-            src: "/dist/src/assets/appIcon.png",
+            src: "/assets/appIcon.png",
             sizes: "192x192",
             type: "image/png",
           },
